8b655fb26fb6a9cedaf636fc17c987d8c4297931
mkaulfers <matthew.kaulfers@yahoo.com>
2023-09-06 14:13:02 -0500

    feat: Add new font and menu options to ContentView
    - Added a new font file "AnalogueOS-Regular.ttf" to the project.
    - Updated the ContentView struct to include a border extension for views.
    - Created an enum MenuOption with different cases representing menu options.
    - Added a side menu in the ContentView with sections for managing and settings.
    - Each menu option is displayed as a NavigationLink with its title and icon.
    - Implemented navigation to destination views based on selected menu option.

--- Rompal/ContentView.swift
+++ Rompal/ContentView.swift
@@ -7,14 +7,147 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @Binding var document: RompalDocument
+extension View {
+    func border(width: CGFloat, edges: [Edge], color: Color) -> some View {
+        overlay(EdgeBorder(width: width, edges: edges).foregroundColor(color))
+    }
+}
 
+struct EdgeBorder: Shape {
+    var width: CGFloat
+    var edges: [Edge]
+
+    func path(in rect: CGRect) -> Path {
+        edges.map { edge -> Path in
+            switch edge {
+            case .top: return Path(.init(x: rect.minX, y: rect.minY, width: rect.width, height: width))
+            case .bottom: return Path(.init(x: rect.minX, y: rect.maxY - width, width: rect.width, height: width))
+            case .leading: return Path(.init(x: rect.minX, y: rect.minY, width: width, height: rect.height))
+            case .trailing: return Path(.init(x: rect.maxX - width, y: rect.minY, width: width, height: rect.height))
+            }
+        }.reduce(into: Path()) { $0.addPath($1) }
+    }
+}
+
+enum MenuOption: String, CaseIterable, Identifiable {
+    case favorites = "Favorites"
+    case sync = "Sync"
+    case add = "Add"
+    case remove = "Remove"
+    case importData = "Import"
+    case exportData = "Export"
+    case settings = "Settings"
+    
+    var id: UUID {
+        UUID()
+    }
+    
+    var title: String {
+        return self.rawValue
+    }
+    
+    var iconSystemName: String {
+        switch self {
+        case .favorites: return "heart"
+        case .sync: return "arrow.triangle.2.circlepath"
+        case .add: return "folder.badge.plus"
+        case .remove: return "folder.badge.minus"
+        case .importData: return "arrow.down.doc"
+        case .exportData: return "arrow.up.doc"
+        case .settings: return "gear"
+        }
+    }
+    
+    static var manageSection: [MenuOption] {
+        return [.favorites, .sync, .add, .remove]
+    }
+    
+    static var settingsSection: [MenuOption] {
+        return [.importData, .exportData, .settings]
+    }
+}
+
+struct SelectedButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .background(.red)
+    }
+}
+
+struct ContentView: View {
+    @Namespace var namespace: Namespace.ID
+    @State private var selectedItem: MenuOption?
+    
     var body: some View {
-        TextEditor(text: $document.text)
+        
+        NavigationSplitView(sidebar: {
+            sideMenu
+        }, detail: {
+            VStack {
+                Text("Welcome to RomPal")
+                    .font(.h1)
+                Text("Select an option to get started.")
+                    .font(.p2)
+            }
+        })
+        .background(Color.black)
+        .foregroundColor(.white)
+    }
+    
+    private var sideMenu: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 8) {
+                    manageSection
+                }
+            }
+            Spacer()
+            settingsSection
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical)
+        .border(width: 1, edges: [.trailing], color: .white.opacity(0.10))
+        .background(Color.black)
+    }
+    
+    private var manageSection: some View {
+        Section(header: Text("Manage").font(.h2)) {
+            ForEach(MenuOption.manageSection, id: \.self) { item in
+                
+                NavigationLink(destination: destinationView(for: item), label: {
+                    Label(item.title, systemImage: item.iconSystemName)
+                        .font(.p1)
+                        .foregroundColor(selectedItem == item ? .black : .white)
+                })
+                .listRowBackground(selectedItem == item ? Color.white : Color.clear)
+                .buttonStyle(.plain)
+            }
+        }
+        .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+    }
+    
+    private var settingsSection: some View {
+        Section(header: Text("Settings").font(.h2)) {
+            ForEach(MenuOption.settingsSection, id: \.self) { item in
+                NavigationLink(destination: destinationView(for: item), label: {
+                    Label(item.title, systemImage: item.iconSystemName)
+                        .font(.p1)
+                        .foregroundColor(selectedItem == item ? .black : .white)
+                })
+                .listRowBackground(selectedItem == item ? Color.white : Color.clear)
+                .buttonStyle(.plain)
+            }
+        }
+        .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+    }
+    
+    func destinationView(for item: MenuOption) -> some View {
+        switch item {
+        default: return AnyView(Text(item.title).font(.p1))
+        }
     }
 }
 
 #Preview {
-    ContentView(document: .constant(RompalDocument()))
+    ContentView()
 }
---
+++ Rompal/Extensions/Font+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Font+Extensions.swift
+//  Rompal
+//
+//  Created by Matthew Kaulfers on 9/6/23.
+//
+
+import SwiftUI
+
+extension Font {
+    // Create static fonts for .h1 .h2 and .p1 and .p2 using AnalogueOS-Regular.ttf
+    // This is the default font for Rompal.
+    static let h1 = Font.custom("AnalogueOS-Regular", size: 24)
+    static let h2 = Font.custom("AnalogueOS-Regular", size: 20)
+    static let p1 = Font.custom("AnalogueOS-Regular", size: 18)
+    static let p2 = Font.custom("AnalogueOS-Regular", size: 14)
+}
